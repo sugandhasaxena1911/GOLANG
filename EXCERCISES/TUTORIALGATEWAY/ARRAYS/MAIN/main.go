bb1afc26a46631b0577d116022f0d337297e1db8
sugandhasaxena19 <sugandhasaxena19@gmail.com>
2023-02-22 15:30:04 +0530

    Please commit docs

---
+++ EXCERCISES/TUTORIALGATEWAY/ARRAYS/MAIN/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"EXCERCISES/TUTORIALGATEWAY/ARRAYS/Operations"
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello")
+	arr1 := Operations.EnterMatrix(3, 3)
+	fmt.Println(arr1)
+	Operations.Print2DArray(arr1)
+	sl := []int{1, 2, 5, 6, 2, 8, 5, 9, 1}
+	dupCnt := Operations.CountDuplicates(sl)
+	fmt.Println("the number of duplicates were ", dupCnt)
+	arr2 := Operations.TransposeMatrix(arr1)
+	fmt.Println("After transpose", arr2)
+
+}
